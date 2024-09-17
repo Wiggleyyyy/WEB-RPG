0eac290246d7b8f071826cd97994195faada92d8
SoloBall <arma0130@edu.sde.dk>
2024-09-17 15:19:22 +0200

    Added random map generation but killed the website.

--- rpg_game/src/app/page.js
+++ rpg_game/src/app/page.js
@@ -1,10 +1,24 @@
+"use client"
 import Image from "next/image";
 import { Navigation } from "@/components/navigation";
 
 export default function Home() {
+  function Move(direction) {
+    switch (direction) {
+      case "up": 
+
+        break;
+      case "left":
+        
+        break;
+    }
+  }
+
   return (
     <main className="bg-zinc-800 w-[100dvw] h-[100dvh] ">
-      <Navigation />
+      <Navigation MoveDirection={Move} />
     </main>
   );
 }
+
+
--- rpg_game/src/components/navigation.js
+++ rpg_game/src/components/navigation.js
@@ -2,61 +2,57 @@
 
 import * as React from "react";
 
-export function Navigation() {
+export function Navigation({MoveDirection}) {
+    function RandInt(max){
+        var rand = Math.floor(Math.random() * max) + 1;
+        const result = [];
+        for (var i = 0; i < rand; i++){
+            result.push(i);
+        }
+        return result;
+    }
+    var rows = RandInt(5);
+    var rowColumns;
+    var squares = 0;
+    /*while (squares < 10){
+        squares = 0;
+        for (var i = 0; i < rows.length; i++){
+            var rand = RandInt(7);
+            rowColumns.push(rand);
+            squares += rand.length;
+            console.log(squares);
+        }
+    }*/
   return (
     <div className="bg-zinc-700 border-2 border-yellow-400 w-[20dvw] h-[60dvh] rounded-lg absolute right-2 bottom-2 flex flex-col justify-center items-center">
         <div className="flex flex-col h-[40%] w-[90%] justify-center items-center">
-            <div className="flex flex-row">
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-            </div>
-            <div className="flex flex-row">
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-            </div>
-            <div className="flex flex-row">
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-yellow-400 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-            </div>
-            <div className="flex flex-row">
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-            </div>
-            <div className="flex flex-row">
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-                <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
-            </div>
+            {rows.map((row) => {
+            return (
+                <div className="flex flex-row">
+                    {rowColumns[row].map((col) => (
+                        <>
+                        <div className="w-[2vw] h-[4vh] bg-zinc-800 m-1"></div>
+                        {(rowColumns[row].length % 2 == 0 && col + 1 == rowColumns[row].length) && <div className="w-[2vw] h-[4vh] m-1"> </div>}
+                        </>
+                    ))}
+                </div>
+            )})}
         </div>
         <div className="w-[90%] h-[0.25vh] bg-yellow-400 m-5"></div>
         <div className="h-[40%] flex flex-col justify-center items-center">
             <div className="flex flex-row">
                 <div className="w-[4vw]"></div>
-                <button className="bg-zinc-800 border-2 border-yellow-400 rounded-lg w-[4vw] p-2 text-yellow-400">Up</button>
+                <button onClick={() => MoveDirection("up")} className="bg-zinc-800 border-2 border-yellow-400 rounded-lg w-[4vw] p-2 text-yellow-400">Up</button>
                 <div className="w-[4vw]"></div>
             </div>
             <div className="flex flex-row">
-                <button className="bg-zinc-800 border-2 border-yellow-400 rounded-lg w-[4vw] p-2 text-yellow-400">Left</button>
+                <button onClick={() => MoveDirection("left")} className="bg-zinc-800 border-2 border-yellow-400 rounded-lg w-[4vw] p-2 text-yellow-400">Left</button>
                 <div className="w-[4vw]"></div>
-                <button className="bg-zinc-800 border-2 border-yellow-400 rounded-lg w-[4vw] p-2 text-yellow-400">Right</button>
+                <button onClick={() => MoveDirection("right")} className="bg-zinc-800 border-2 border-yellow-400 rounded-lg w-[4vw] p-2 text-yellow-400">Right</button>
             </div>
             <div className="flex flex-row">
                 <div className="w-[4vw]"></div>
-                <button className="bg-zinc-800 border-2 border-yellow-400 rounded-lg w-[4vw] p-2 text-yellow-400">Down</button>
+                <button onClick={() => MoveDirection("down")} className="bg-zinc-800 border-2 border-yellow-400 rounded-lg w-[4vw] p-2 text-yellow-400">Down</button>
                 <div className="w-[4vw]"></div>
             </div>
         </div>
