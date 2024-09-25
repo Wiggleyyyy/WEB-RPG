425cec915d0bb47af1cc51e9a70876fe8a056496
David <141311953+Wiggleyyyy@users.noreply.github.com>
2024-09-25 12:58:43 +0200

    added current tiles for interactions

--- rpg_game/src/components/interactions.js
+++ rpg_game/src/components/interactions.js
@@ -1,9 +1,23 @@
 "use client";
 
-export function Interaction(tileType) {
+export function Interaction({tileType}) {
   return (
     <div className="w-[20vw] h-[37.5vh] bg-zinc-700 border-2 border-yellow-400 absolute right-2 top-2 rounded-lg">
-        
+        {tileType === "plain" ? (
+            <h1>plain</h1>
+        ) : tileType === "settlement" ? (
+            <h1>settlement</h1>
+        ) : tileType === "forest" ? (
+            <h1>forest</h1>
+        ) : tileType === "dungeon" ? (
+            <h1>dungeon</h1>
+        ) : tileType === "capital" ? (
+            <h1>capital</h1>
+        ) : tileType === "lake" ? (
+            <h1>lake</h1>
+        ) : (
+            <h1>Error, tile type not found</h1>
+        )}
     </div>
   );
 }
